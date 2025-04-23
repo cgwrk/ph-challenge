bdb292fbf8b5703c0c2c9b434f60e576ae9f6830
Corey Giovanella <corey@giovanella.ca>
2025-04-23 13:15:18 -0400

    Improved docs, fixed cleaned up responses

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # dicomd 
 Errors or Issues I have thought of have been left as a comment in the code with a FIXME keyword, though fixing some of them are outside of my ability.
 
-I used Image Magick for the conversion of the dicom file into the png.  Until this point I had been using alpine for my base image, however it looks like the alpine ImageMagick package is lacking dicom support, so I migrated to ubuntu.
+I used ImageMagick for the conversion of the dicom file into the png.  Until this point I had been using alpine for my base image, however it looks like the alpine ImageMagick package is lacking dicom support, so I migrated to ubuntu.
 
 # Build
-How to build the container
+How to build the container.  I used podman, but docker should work just as well.
 
 ```
 podman build -t dicomd:v2 .
@@ -16,41 +16,71 @@ podman run -ti --rm -p 5000:8080 dicomd:v2
 ```
 
 # Endpoints and general architecture
-I don't like how I layed out the end points.  There seems to be a lack of upper level objects, but then again it is a fairly simple implementation.
+I had initially asked if I needed to include a path of the uploaded file, which I was told I could simplify this assume the server only worked on 1 file at a time.  I realized there were issues with that.  For example if 2 clients used this api at the same time it pretty much guaranteed there would be a problem with file collision.  It also would have been severely limiting for future expandability.  Because of this, I put the effort into making the system work on path.  It wasn’t as complicated as I thought it does not remove collision on files being worked on it reduces the chance.
+
+# Usage:
+To upload a file.  POST
+```
+<BASE_URL>/api/v1/upload/<path>/<you>/<want>/filename
+```
+To get all tags in a given file.  GET
+```
+<BASE_URL>/api/v1/tags/<path>/<you>/<want>/filename
+```
+to get a specific tag.  GET
+```
+<BASE_URL>/api/v1/tags/<path>/<you>/<want>/filename?tag=<tag>
+```
+where tag is formatted as <4 hex char>,<4 hex char>.  Eg, 1234,5678
+To get a png if the image.  GET
+```
+<BASE_URL>/api/v1/image/<path>/<you>/<want>/filename DELETE
+```
+To clean up and remove the image from the micro service 
+
 
 # Testing/Usage examples
 
 Upload a valid image file, get all tags from it, then get a single tag.
 ```
-http --form POST http://localhost:5000/api/v1/upload  file@MRI/PA000001/ST000001/SE000001/IM000001
-http GET http://localhost:5000/api/v1/tag
-http GET http://localhost:5000/api/v1/tag/0051,100e
-# in browser get png by loading http://localhost:5000/api/v1/image
+http --form POST http://localhost:5000/api/v1/upload/MRI/PA000001/ST000001/SE000001/IM000001  file@MRI/PA000001/ST000001/SE000001/IM000001
+http GET http://localhost:5000/api/v1/tags/MRI/PA000001/ST000001/SE000001/IM000001
+http GET http://localhost:5000/api/v1/tags/MRI/PA000001/ST000001/SE000001/IM000001?tag=0051,100e
+# in browser get png by loading http://localhost:5000/api/v1/image/MRI/PA000001/ST000001/SE000001/IM000001
 
 ````
 
-Try to upload another file, verify failure, remove it.
+Try to upload another file to same location, verify failure, remove it.
 ```
-http --form POST http://localhost:5000/api/v1/upload  file@MRI/PA000001/ST000001/SE000001/IM000002
-http DELETE http://localhost:5000/api/v1/remove
+http --form POST http://localhost:5000/api/v1/upload/MRI/PA000001/ST000001/SE000001/IM000001  file@MRI/PA000001/ST000001/SE000001/IM000001
+http DELETE http://localhost:5000/api/v1/remove/MRI/PA000001/ST000001/SE000001/IM000001
 ```
 
 Upload a non-image dicom file, get some tags, then try to get a png from it.
 ```
-http --form POST http://localhost:5000/api/v1/upload  file@XRAY/DICOMDIR
-http GET http://localhost:5000/api/v1/tag/0004,1212
-http GET http://localhost:5000/api/v1/image
+http --form POST http://localhost:5000/api/v1/upload/XRAY/DICOMDIR  file@XRAY/DICOMDIR
+http GET http://localhost:5000/api/v1/tags/XRAY/DICOMDIR
+http GET http://localhost:5000/api/v1/tags/XRAY/DICOMDIR?tag=0004,1212
+http GET http://localhost:5000/api/v1/image/XRAY/DICOMDIR
 ```
 
 Try to upload an invalid file
 ```
-http --form POST http://localhost:5000/api/v1/upload  file@src/dicomd.py
+http --form POST http://localhost:5000/api/v1/upload/src/dicomd.py file@src/dicomd.py
 ```
 
 Get the image header, just because it's big binary and may break something.
 ```
-http GET http://localhost:5000/tag/0029,1010
+http --form POST http://localhost:5000/api/v1/upload/MRI/PA000001/ST000001/SE000001/IM000001  file@MRI/PA000001/ST000001/SE000001/IM000001
+http GET http://localhost:5000/api/v1/tags/MRI/PA000001/ST000001/SE000001/IM000001?tag=0029,1010
 ```
 
-
+# Issues and possible improvements
+- ../ in paths do not provide a reasonable error message if they are early in the path, and go through and work as you’d expect .. would if later on in the file system.  I’m not sure how I feel about this, but I’d say its not ideal.  Probably completely banning relative path information would be better.
+- directory structure does not clean up empty directories ever
+- there is likely a file length limitation on the uploaded image, but I don’t know what it is or hwo the code will handle a file that hits this limit
+- still running the dev we server
+- 2 clients could try to work on the same file path at the same time and get in the way of each other without knowing it.
+- I’m not entirely sure if the url responses should produce hyper links.  I generally think the output I have produced as responses is lacking.  More for human interaction then a system.
+- What happens if someone puts an incredibly long path in the name.  Likely something bad that is not handled.  I see a problem here.  This may not have been a good idea not to restrict the length
 
--- src/dicomd.py
+++ src/dicomd.py
@@ -10,30 +10,25 @@ from wand.image import Image
 #FIXME.  There likely is a maximum length on file that can be uploaded.  I don't know what that is.
 #FIXME.  What if 2 clients use this api at once?  chaos.  Some form of Auth and tracy by user? Path included in filename would work, but outside my ability at the moment.
 
-#FIXME.  write api and build docs
-
 port=8080
 rootDir="/data/"
 apiVersion='/api/v1'
 
-#As I am only handling 1 file at a time, I'll hard code it's internal path.  This is not ideal.
-internalFile = rootDir + 'current_dicom_file.dcm'
-internalFileImg = rootDir + 'current_dicom_file.png'
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 # I am unsure if uploading a file like this is valid Rest. 
 # base64 encoding the file at the client and decoding it at the server maybe more valid, but would have more overhead on client and server.
 @app.route(apiVersion + '/upload/<path:path>', methods=['POST'])
 def upload(path):
   if 'file' not in request.files:
-    resp = jsonify({'message': 'No file part in the request.  You need pass in a file to upload.'})
+    resp = jsonify({'message': 'No file part in the request.  You need pass in a file to upload to this path.'})
     resp.status_code = 400
     return resp
     
   data = request.files['file']
 
-  #FIXME: will this code ever run?  I don't think so, maybe empty filename?  test it?  A clinet could pass it along.
+  #FIXME: will this code ever run?  I don't think so, maybe empty filename?  test it?  A clinet could pass it along?
   if data.filename == '':
     resp = jsonify({'message': 'No file selected for uploading'})
     resp.status_code = 400
@@ -43,8 +38,10 @@ def upload(path):
     #FIXME:  what if someone has something evil in filename.  I'm not sure this matters here.
 #    filename = secure_filename(data.filename) 
     filename = secure_filename(rootDir + path) 
+    print(filename)
     if os.path.exists(rootDir + path):
-      resp = jsonify({'message': 'file ' + path + ' already exists.  Please remove it first.'})
+      resp = jsonify({'message': 'file ' + path + ' already exists.  Please remove it first.',
+                      'file': path})
       resp.status_code = 400
       return resp
     os.makedirs(os.path.dirname(rootDir + path), exist_ok=True)
@@ -53,14 +50,17 @@ def upload(path):
       ds = pydicom.dcmread(rootDir + path)
     except:
       os.remove(rootDir + path)
-      resp = jsonify({'message': 'Uploaded file ' + path + ' does not appear to be a valid dicom file.  Please troubleshoot and re-upload it.'})
+      resp = jsonify({'message': 'Uploaded file does not appear to be a valid dicom file.  Please troubleshoot and re-upload it.',
+                      'file': path})
       resp.status_code = 400
       return resp
 
-    resp = jsonify({'message': 'File ' + path + ' successfully uploaded'})
+    resp = jsonify({'message': 'File successfully uploaded',
+                    'file': path})
     resp.status_code = 201
     return resp
-    
+  #catch all should not run 
+  resp = jsonify({'message': 'Unknown error uploading file.  Please contact support.'})
   resp.status_code = 400
   return resp
 
@@ -71,14 +71,15 @@ def remove(path):
     os.remove(rootDir + path)
     #try to clean up the image incase it got created
     if os.path.exists(rootDir + path + '.png'):
-      os.remove(filename=rootDir + path + '.png')
-    resp = jsonify({'message': 'File ' + path + ' was removed.'})
+      os.remove(rootDir + path + '.png')
+    resp = jsonify({'message': 'File was removed.',
+                    'file': path})
     resp.status_code = 200
     return resp
-  resp = jsonify({'message': 'File ' + path + ' does not exist.  It may have already been removed.'})
+  resp = jsonify({'message': 'File does not exist.  It may have already been removed.',
+                  'file': path})
   resp.status_code = 400
   return resp
-
 
 @app.route(apiVersion + '/tags/<path:path>', methods=['GET'])
 def tag(path):
@@ -91,33 +92,33 @@ def tag(path):
     resp.status_code = 400
     return resp
 
-#  if not tag:
-#    resp = jsonify({'message': 'No tag part in the request'})
-#    resp.status_code = 400
-#    return resp
-#  if tag == '*':
-
   # if no tag specified, display all tags options
   if not tag:
     taglist = []
     for x in ds:
       taglist.append(str(x.tag)[1:-1].replace(" ", ""))
     resp = jsonify({'message': 'These are the avalable tags for the uploaded file.  Please append it to the end after the tag in your url.  EG ?tag=0010,0001',
-                    'avalable tags': taglist
+                    'file': path,
+                    'tags': taglist
                    })
+
     resp.status_code = 200
     return resp
   else:
     tag = tag.lower()
-
+    #verify correct format passed in
     if re.match('^[0-9a-f]{4}\,[0-9a-f]{4}$', tag):
       formattedTag = '0x' + tag[0:4] + tag[5:]
     else:
-      resp = jsonify({'message': 'invalid tag.  Tags have to be two sets 4 hex charicters, seporated by a comma.  EG ?tag=0010,0001' })
+      resp = jsonify({'message': 'Invalid tag.  Tags have to be two sets 4 hex charicters, seporated by a comma.  EG ?tag=0010,0001',
+                      'file': path })
       resp.status_code = 400
       return resp
     
-  resp = jsonify(ds[formattedTag].to_json())
+  resp = jsonify({'message': 'All tag values',
+                 'file': path,
+                 'taginfo': ds[formattedTag].to_json() })
+  #FIXME: I'm not a fan of taginfo's formatting.
   resp.status_code = 200
   return resp
 
@@ -128,7 +129,8 @@ def getImage(path):
   try:
     ds = pydicom.dcmread(rootDir + path)
   except:
-    resp = jsonify({'message': 'File ' + path + ' has not been uploaded yet.  Please upload it first.'})
+    resp = jsonify({'message': 'File has not been uploaded yet.  Please upload it first.',
+                    'file': path})
     resp.status_code = 400
     return resp
   try:
@@ -136,19 +138,22 @@ def getImage(path):
       img.save(filename=rootDir + path + '.png')
     return send_file(rootDir + path + '.png', mimetype='image/png')
   except:
-    resp = jsonify({'message': 'Something went wrong in converting the image ' + path + ',  The uploaded file may not even be an image.  Please investigate.'})
+    resp = jsonify({'message': 'Something went wrong in converting the image The uploaded file may not even be an image.  Please investigate and contact support if needed.',
+                    'file': path})
     resp.status_code = 400
     return resp
 
 @app.route('/')
+@app.route('/api')
+@app.route(apiVersion)
 def index():
-#FIXME, output better usage
-  return json.dumps({'endpoints': [apiVersion + "upload",
-                                  apiVersion + "tag",
-                                  apiVersion + "image"]
+  #FIXME, output better usage
+  return json.dumps({'message': 'Endpoints listed below with rough usage',
+                     'endpoints': [apiVersion + "upload/<as>/<much>/<path>/<as>/<you>/<like>/filename",
+                                  apiVersion + "tags/<as>/<much>/<path>/<as>/<you>/<like>/filename",
+                                  apiVersion + "image/<as>/<much>/<path>/<as>/<you>/<like>/filename",
+                                  apiVersion + "delete/<as>/<much>/<path>/<as>/<you>/<like>/filename"]
                    })
-# json.dumps({'endpoints': apiVersion + 'upload'})
-# Endpoints
 
 #FIXME, just for testing, not prod.
 app.run(host='0.0.0.0', port=port, debug=True)
